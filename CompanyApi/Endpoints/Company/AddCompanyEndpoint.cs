848a7b61812a7859d5633e356374dbfd597e9482
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-21 16:15:26 +0430

    Add AddCompanyEndpoint

---
+++ CompanyApi/Endpoints/Company/AddCompanyEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using CompanyApi.Models.Contracts;
+using CompanyApi.Services;
+
+namespace CompanyApi.Endpoints.Company;
+
+public class AddCompanyEndpoint : Endpoint<AddUpdateCompanyRequest>
+{
+	private readonly ICompanyService companyService;
+
+	public AddCompanyEndpoint(ICompanyService companyService)
+	{
+		this.companyService = companyService;
+	}
+	public override void Configure()
+	{
+		Post();
+		Routes("/companies");
+		AllowAnonymous();
+	}
+
+	public override async Task HandleAsync(AddUpdateCompanyRequest req, CancellationToken ct)
+	{
+		await companyService.Add(req);
+		await SendAsync(new EmptyResponse(), StatusCodes.Status201Created, cancellation: ct);
+	}
+}
---
+++ CompanyApi/Validators/Company/AddUpdateCompanyValidator.cs
@@ -0,0 +1,27 @@
+﻿using CompanyApi.Models.Contracts;
+using FastEndpoints.Validation;
+using FluentValidation;
+
+namespace CompanyApi.Validators;
+
+public class AddUpdateCompanyValidator : Validator<AddUpdateCompanyRequest>
+{
+	public AddUpdateCompanyValidator()
+	{
+		RuleFor(auc => auc.Name)
+			.NotEmpty().WithMessage("is required!")
+			.MaximumLength(50).WithMessage("length should be 50 or less!");
+
+		RuleFor(auc => auc.City)
+			.NotEmpty().WithMessage("is required!")
+			.MaximumLength(50).WithMessage("length should be 50 or less!");
+
+		RuleFor(auc => auc.State)
+			.NotEmpty().WithMessage("is required!")
+			.MaximumLength(50).WithMessage("length should be 50 or less!");
+
+		RuleFor(auc => auc.PostalCode)
+			.NotEmpty().WithMessage("is required!")
+			.MaximumLength(15).WithMessage("length should be 15 or less!");
+	}
+}

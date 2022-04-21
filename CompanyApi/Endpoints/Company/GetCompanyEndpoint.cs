761ee1002f6ff9f9a31ba8c68b34aa053350fefd
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-21 16:46:55 +0430

    Add GetCompanyEndpoint

---
+++ CompanyApi/Endpoints/Company/GetCompanyEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using CompanyApi.Mappers.EndpointMappers;
+using CompanyApi.Models.Contracts;
+using CompanyApi.Services;
+
+namespace CompanyApi.Endpoints.Company;
+
+public class GetCompanyEndpoint : Endpoint<EmptyRequest, GetCompanyResponse, CompanyEndpointMapper>
+{
+	private readonly ICompanyService companyService;
+
+	public GetCompanyEndpoint(ICompanyService companyService)
+	{
+		this.companyService = companyService;
+	}
+
+	public override void Configure()
+	{
+		Get();
+		Routes("companies/{id:int}");
+		AllowAnonymous();
+	}
+
+	public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+	{
+		var id = Route<int>("id");
+		var company = await companyService.GetCompany(id);
+
+		if (company is null)
+			await SendNotFoundAsync(cancellation: ct);
+
+		var response = Map.FromEntity(company);
+		await SendAsync(response, cancellation: ct);
+	}
+}

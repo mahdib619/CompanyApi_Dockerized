bfeab6420bdba007a1ff4e19d71173c69c94c05a
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-21 16:24:21 +0430

    Add Id to GetCompanyResponse

--- CompanyApi/Models/Contracts/Response/GetCompanyResponse.cs
+++ CompanyApi/Models/Contracts/Response/GetCompanyResponse.cs
@@ -2,6 +2,7 @@ namespace CompanyApi.Models.Contracts;
 
 public class GetCompanyResponse
 {
+	public int Id { get; set; }
 	public string Name { get; set; }
 	public string Address { get; set; }
 	public string City { get; set; }

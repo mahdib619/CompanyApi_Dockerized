9bfbd95f66b88ce0085b170d271779659216cbfb
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-21 17:32:18 +0430

    Add UpdateCompanyEndpoint

---
+++ CompanyApi/Endpoints/Company/UpdateCompanyEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using CompanyApi.Models.Contracts;
+using CompanyApi.Services;
+
+namespace CompanyApi.Endpoints.Company;
+
+public class UpdateCompanyEndpoint : Endpoint<AddUpdateCompanyRequest>
+{
+	private readonly ICompanyService companyService;
+
+	public UpdateCompanyEndpoint(ICompanyService companyService)
+	{
+		this.companyService = companyService;
+	}
+	public override void Configure()
+	{
+		Post();
+		Routes("/companies/{id:int}");
+		AllowAnonymous();
+	}
+
+	public override async Task HandleAsync(AddUpdateCompanyRequest req, CancellationToken ct)
+	{
+		var id = Route<int>("id");
+
+		if (await companyService.Update(id, req))
+			await SendAsync(new EmptyResponse(), cancellation: ct);
+		else
+			await SendNotFoundAsync(cancellation: ct);
+	}
+}

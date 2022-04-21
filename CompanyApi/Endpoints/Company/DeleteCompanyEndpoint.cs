418f6df27b4167b43eb8b83853f714c3b36df372
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-21 17:19:01 +0430

    Add DeleteCompanyEndpoint

---
+++ CompanyApi/Endpoints/Company/DeleteCompanyEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using CompanyApi.Mappers.EndpointMappers;
+using CompanyApi.Services;
+
+namespace CompanyApi.Endpoints.Company;
+
+public class DeleteCompanyEndpoint : Endpoint<EmptyRequest, EmptyResponse, CompanyEndpointMapper>
+{
+	private readonly ICompanyService companyService;
+
+	public DeleteCompanyEndpoint(ICompanyService companyService)
+	{
+		this.companyService = companyService;
+	}
+
+	public override void Configure()
+	{
+		Delete();
+		Routes("/companies/{id:int}");
+		AllowAnonymous();
+	}
+
+	public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+	{
+		var id = Route<int>("id");
+		
+		if(await companyService.Remove(id))
+			await SendAsync(new EmptyResponse(), cancellation: ct);
+		else
+			await SendNotFoundAsync(cancellation: ct);
+	}
+}

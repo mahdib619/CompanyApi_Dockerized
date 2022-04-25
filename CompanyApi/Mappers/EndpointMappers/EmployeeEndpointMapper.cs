67f7dbc6bb892f9bb40eb17e0156f7719f384255
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-25 21:12:21 +0430

    Add Employee mappers

---
+++ CompanyApi/Mappers/EndpointMappers/EmployeeEndpointMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CompanyApi.Models.Contracts;
+
+namespace CompanyApi.Mappers.EndpointMappers;
+
+public class EmployeeEndpointMapper<TRequest> : Mapper<TRequest, GetEmployeeResponse, Employee> where TRequest : new()
+{
+	private readonly IMapper autoMapper;
+	public EmployeeEndpointMapper()
+	{
+		autoMapper = RegisterServices.ServiceProvider.GetService<IMapper>();
+	}
+
+	public override GetEmployeeResponse FromEntity(Employee e)
+	{
+		return autoMapper.Map<GetEmployeeResponse>(e);
+	}
+
+	public override Employee ToEntity(TRequest r)
+	{
+		return autoMapper.Map<Employee>(r);
+	}
+}
---
+++ CompanyApi/Mappers/Profiles/EmployeeMapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CompanyApi.Models.Contracts;
+
+namespace CompanyApi.Mappers.Profiles;
+
+public class EmployeeMapProfile : Profile
+{
+	public EmployeeMapProfile()
+	{
+		CreateMap<AddEmployeeRequest, Employee>();
+		CreateMap<UpdateEmployeeRequest, Employee>();
+		CreateMap<Employee, GetEmployeeResponse>();
+	}
+}

ddb461ead7c7016c5a92921260b069f405be9920
Mehdi Boghraei <mahdib619@gmail.com>
2022-04-28 21:17:53 +0430

    Add ToDynamic Extension

---
+++ CompanyApi/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace CompanyApi;
+
+public static class Extensions
+{
+	public static object ToAnonymous<T>(this T obj, params Expression<Func<T, object>>[] excludeExpressions)
+	{
+		var excluded = excludeExpressions
+			.Select(a =>
+			{
+				if(a.Body is MemberExpression member)
+					return member.Member.Name;
+
+				if (a.Body is UnaryExpression unary && unary.Operand is MemberExpression memExp)
+					return memExp.Member.Name;
+
+				return null;
+			})
+			.Where(a => a is not null).ToList();
+
+		var props = obj.GetType().GetProperties().Where(p => !excluded.Contains(p.Name));
+		var output = new ExpandoObject();
+
+		foreach (var prop in props)
+			output.TryAdd(prop.Name, prop.GetValue(obj));
+
+		return output;
+	}
+}
